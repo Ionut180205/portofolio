67cd8403a80646dda545a4c74f1a151d72180ce9
Ionut180205 <ionut.cioarsa@gmail.com>
2025-01-26 18:50:28 +0200

    THANOS

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portofolio', // Base URL for deployment, update if deploying to a subpath
+  base: '/portofolio/', // Base URL for deployment, update if deploying to a subpath
   build: {
     outDir: 'dist', // Customize this if you want a different output directory
   },
   preview: {
     // This will make sure the preview server uses the correct base path
-    base: '/portofolio',
+    base: '/portofolio/',
   }
 });
